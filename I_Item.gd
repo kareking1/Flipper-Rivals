153993a29c3e200d0f3be59bf571642fe742f221
Karekin <karekin.kiyici@dawsoncollege.qc.ca>
2024-02-01 13:26:20 -0500

    Fixed up Items and created a txt as a guid for traits and types

--- I_Item.gd
+++ I_Item.gd
@@ -5,30 +5,20 @@ class_name  I_Item #Makes this script inheritable
 @export var item_name = ""
 @export var base_value = 0.0
 @export var actual_value = 0.0
-@export var umbrella_type = ""
-@export var specific_type = ""
+@export var type = ""
 @export var traits_list = []
-var on_sale = false #Mostly included these two bool vars for if we need to quickly check if an item is changed... make sure to update them!
-var sale_percentage = 1.0
-var price_increased = false
-var price_increase_amount = 0.0
 
-func calculate_final_price():
+func calculate_final_price(sale_percentage: float, price_increase_amount: float):
 	actual_value = base_value * sale_percentage + price_increase_amount
 
 func get_item_basic_details():
-	return "-Name: " + item_name + "\n-Value: " + str(actual_value) + "\n-Types: " + umbrella_type + ", " + specific_type
+	return "-Name: " + item_name + "\n-Value: " + str(actual_value) + "\n-Type: " + type
 
-func set_values(i_name:String, base_v:float, sale:bool, sale_per:float, price_inc:bool, price_inc_amount:float, u_type:String, s_type:String, traits:Array):
+func set_values(i_name:String, base_v:float, typ:String, traits:Array):
 	item_name = i_name
 	base_value = base_v
-	on_sale = sale
-	price_increased = price_inc
-	sale_percentage = sale_per
-	price_increase_amount = price_inc_amount
-	calculate_final_price()
-	umbrella_type = u_type
-	specific_type = s_type
+	actual_value = base_v
+	type = typ
 	for trt:String in traits:
 		traits_list.append(trt)
 
