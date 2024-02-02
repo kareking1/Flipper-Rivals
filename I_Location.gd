556539989341e0c05535a82d5c1bec123de4fc7b
Karekin <karekin.kiyici@dawsoncollege.qc.ca>
2024-02-02 12:10:04 -0500

    Made Items and Locations easier to use now and changed Market accordingly

--- I_Location.gd
+++ I_Location.gd
@@ -1,12 +1,11 @@
-extends Node
-class_name I_Location #Look at I_Item.gd for inheritance details
+class_name I_Location
 
-@export var location_name = ""
-@export var base_population = 0
-@export var actual_population = 0
-@export var base_shop_setup_price = 0.0
-@export var actual_shop_setup_price = 0.0
-@export var current_items_of_interest = []
+var location_name = ""
+var base_population = 0
+var actual_population = 0
+var base_shop_setup_price = 0.0
+var actual_shop_setup_price = 0.0
+static var current_items_of_interest = []
 
 func get_location_basic_details():
 	return "-Name: " + location_name + "\n-Population: " + actual_population + "\n-Shop Setup Price: " + actual_shop_setup_price
@@ -19,6 +18,21 @@ func get_location_items_of_interest():
 		return "No items of particular interest here!"
 	return return_string
 	
-#We are going to need to make a customer script as well,
-#but that might not need to be inheritable unless we want
-#the same list of customers that we will represent.
+func set_values(loc_name: String, base_pop: int, base_shop_price: float, items_of_interest: Array = []):
+	location_name = loc_name
+	base_population = base_pop
+	actual_population = base_pop
+	base_shop_setup_price = base_shop_price
+	actual_shop_setup_price = base_shop_price
+	if items_of_interest.is_empty():
+		return
+	for interest in items_of_interest:
+		current_items_of_interest.append(interest)
+	
+static func get_current_interests():
+	return current_items_of_interest
+	
+static func set_current_interests(new_interests: Array):
+	current_items_of_interest.clear()
+	for interest in new_interests:
+		current_items_of_interest.append(interest)
